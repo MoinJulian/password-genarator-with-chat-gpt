064e78fe765700a26eafc7e325b0c5f40eafa698
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-07 18:54:37 +0200

    Updated Code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-A simple password generator completly made with Chat GPT promts
+# Password Generator with Chat GPT
+
+A simple password generator 100% made with Chat GPT promts, the code was copy pasted
 
 [Password Gen](https://password.moinjulian.com)
--- script.js
+++ script.js
@@ -1,30 +1,57 @@
-function generatePassword() {
-  const length = document.getElementById("length").value;
-  const includeNumbers = document.getElementById("includeNumbers").checked;
-  const includeSpecialChars = document.getElementById(
-    "includeSpecialChars"
-  ).checked;
-  const includeUppercase = document.getElementById("includeUppercase").checked;
+document.addEventListener("DOMContentLoaded", function () {
+  const generateButton = document.getElementById("generate-btn");
+  const copyButton = document.getElementById("copy-btn");
+  const generatedPasswordInput = document.getElementById("generated-password");
+  const passwordLengthInput = document.getElementById("password-length");
+  const includeNumbersInput = document.getElementById("include-numbers");
+  const includeSymbolsInput = document.getElementById("include-symbols");
+  const confirmationMessage = document.getElementById("confirmation-message");
 
-  let charset = "abcdefghijklmnopqrstuvwxyz";
-  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (includeNumbers) charset += "0123456789";
-  if (includeSpecialChars) charset += "!@#$%^&*()_-+=";
+  function generatePassword(length, includeNumbers, includeSymbols) {
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  let password = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
+    if (includeNumbers) charset += "0123456789";
+    if (includeSymbols) charset += "!@#$%^&*()_-+={}[]|:;<>,.?/~";
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset.charAt(randomIndex);
+    }
+
+    return password;
   }
 
-  document.getElementById("password").value = password;
-}
+  generateButton.addEventListener("click", function () {
+    const passwordLength = parseInt(passwordLengthInput.value);
+    const includeNumbers = includeNumbersInput.checked;
+    const includeSymbols = includeSymbolsInput.checked;
 
-function copyPassword() {
-  const passwordInput = document.getElementById("password");
-  passwordInput.select();
-  passwordInput.setSelectionRange(0, 99999); // Für mobile Geräte
+    // Validate the input
+    if (passwordLength < 1) {
+      alert("Please enter a valid password length (at least 1).");
+      return;
+    }
 
-  document.execCommand("copy");
-  alert("Password copied to clipboard!");
-}
+    const generatedPassword = generatePassword(
+      passwordLength,
+      includeNumbers,
+      includeSymbols
+    );
+    generatedPasswordInput.value = generatedPassword;
+  });
+
+  copyButton.addEventListener("click", function () {
+    generatedPasswordInput.select();
+    document.execCommand("copy");
+
+    // Remove the selection (highlight) from the input field
+    window.getSelection().removeAllRanges();
+
+    // Show confirmation message
+    confirmationMessage.textContent = "Password copied to clipboard!";
+    setTimeout(() => {
+      confirmationMessage.textContent = "";
+    }, 3000);
+  });
+});
